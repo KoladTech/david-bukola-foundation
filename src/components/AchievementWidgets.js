9f02076afacb94103de671b6072c0825b73dab7a
Kayode Kolawole <sirkay1001@gmail.com>
2024-11-09 23:20:35 +0100

    complete implementation of some sections on Homepage
    This commit involved, ensuring the Name, motto, image, about us and achievement sections were completed

--- src/components/AchievementWidgets.js
+++ src/components/AchievementWidgets.js
@@ -1,9 +1,9 @@
 export default function AchievementWidgets() {
   return (
     // {View AchievementWidgets button}
-    <div>
+    <div className="absolute bottom-8 right-12">
       <a
-        href="/donate"
+        href="/"
         className="inline-block bg-blue-500 hover:bg-blue-600 text-white text-sm font-medium px-4 py-2 rounded-md transition-colors"
       >
         View Achievements
