9f3513857e8786ed606d0d8f9e37315c793e4056
Kayode Kolawole <sirkay1001@gmail.com>
2024-11-12 00:44:02 +0100

    switch the meet the team and get involved sections

--- src/components/GetInvolved.js
+++ src/components/GetInvolved.js
@@ -3,7 +3,7 @@ import PictureCard from "./PictureCard";
 
 export default function GetInvolved() {
   return (
-    <section className="py-2 px-2 mt-4">
+    <section className="py-2 px-2 my-8">
       <div className="max-w-6xl mx-auto my-4">
         <h2 className="text-4xl font-semibold mb-6 text-center">
           Get Involved
