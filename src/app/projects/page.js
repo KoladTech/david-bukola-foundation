637e58629f53a27fa00a23c13e36ecb0858d959b
Kayode Kolawole <sirkay1001@gmail.com>
2024-11-16 11:22:03 +0100

    feat(New page): Finalize design of the project page
    Adds new project page to the site and changes the desiogn of the conetent card to accomamodate the required design, also includes minor design changes to the footer

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <div className="flex flex-col gap-2 my-4">
       {/* Hero Section */}
-      <div className="flex flex-col ">
+      <div className="flex flex-col">
         <HeroSection
           className=""
           title="PROJECTS"
@@ -18,10 +18,12 @@ export default function Page() {
       </div>
       {/* Project Sections  */}
       <div className="flex flex-col space-y-10 mb-24">
+        <h3 className="text-3xl font-bold">Coming soon</h3>
         <div>
           <ContentCard
             className=""
             title="Borehole Project"
+            subtitle="Timeline: 2 Months"
             content="Lorem ipsum dolor sit amet consectetur. Consectetur massa eu pharetra porttitor nulla ornare. Cursus senectus id ipsum cras cursus. Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu. Lorem ipsum dolor sit amet consectetur. Consectetur massa eu pharetra porttitor nulla ornare. Cursus senectus id ipsum cras cursus. Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu.
 Lorem ipsum dolor sit amet consectetur. Consectetur massa eu pharetra porttitor nulla ornare. Cursus senectus id ipsum cras cursus. Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu.
 Lorem ipsum dolor sit amet consectetur. Consectetur massa eu pharetra porttitor nulla ornare. Cursus senectus id ipsum cras cursus. Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu. Lorem ipsum dolor sit amet consectetur. Consectetur massa eu pharetra porttitor nulla ornare. Cursus senectus id ipsum cras cursus. Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu."
