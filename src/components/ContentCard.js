a692957394b775695c8a5222fb63d32ea77c8660
Kayode Kolawole <sirkay1001@gmail.com>
2024-11-16 11:22:03 +0100

    feat(New page): Finalize design of the project page
    Adds new project page to the site and changes the desiogn of the conetent card to accomamodate the required design, also includes minor design changes to the footer

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,11 +1,32 @@
-export default function ContentCard({ title, content }) {
+// export default function ContentCard({ title, subtitle, content }) {
+//   return (
+//     <>
+//       {/* Left Side - Text */}
+//       <div className="w-full justify-center items-center">
+//         <h2 className="text-2xl font-bold mb-4">{title}</h2>
+//         {subtitle && (
+//           <span className="inline">
+//             <h3 className=" text-xl font-semibold text-gray-700">{subtitle}</h3>
+//           </span>
+//         )}
+//         <p className="text-gray-600">{content}</p>
+//       </div>
+//     </>
+//   );
+// }
+
+export default function ContentCard({ title, subtitle, content }) {
   return (
-    <>
-      {/* Left Side - Text */}
-      <div className="w-full justify-center items-center">
-        <h2 className="text-2xl font-bold mb-4">{title}</h2>
-        <p className="text-gray-600">{content}</p>
+    <div className={"space-y-4"}>
+      <div className="flex flex-col sm:flex-row sm:items-baseline sm:space-x-4">
+        <h2 className="text-3xl font-bold">{title}</h2>
+        {subtitle && (
+          <h3 className="text-xl font-semibold text-gray-700 mt-2 sm:mt-0">
+            {subtitle}
+          </h3>
+        )}
       </div>
-    </>
+      <p className="text-gray-600">{content}</p>
+    </div>
   );
 }
