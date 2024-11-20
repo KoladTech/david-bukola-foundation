eb770a537ce7cd1e335e209169e5ba86c031c646
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2024-11-20 17:43:36 +0100

    setup firebase and test creating documents
    Firebase configuration was setup and a script was included to run commands on the database

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,32 @@
+import { config } from "dotenv";
+config({ path: ".env.local" });
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCD2NQWNVTbrvh0z-Xp8kQ0QpslbImdUpE",
+  authDomain: "davidbukolafoundation-db.firebaseapp.com",
+  projectId: "davidbukolafoundation-db",
+  storageBucket: "davidbukolafoundation-db.firebasestorage.app",
+  messagingSenderId: "292529393801",
+  appId: "1:292529393801:web:5eb4af6661b4bcb2653658",
+  measurementId: "G-NMKWPKVKMQ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
