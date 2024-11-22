ee4b70a72bcc78e3f113ca25c008b9b0db545e5d
Kayode Kolawole <sirkay1001@gmail.com>
2024-11-22 12:18:45 +0100

    create a production ready branch for push
    This commit reverts specific changes in the main branch, such as the achievements and projects pages, to ensure only intended updates are deployed to production.

--- src/app/achievements/page.js
+++ src/app/achievements/page.js
@@ -1,6 +1,7 @@
 import ContentCard from "@/components/ContentCard";
 import HeroSection from "@/components/HeroSection";
 import PictureCard from "@/components/PictureCard";
+import UnderConstruction from "@/components/UnderConstruction";
 
 export default function Page() {
   let content = `Lorem ipsum dolor sit amet consectetur. Consectetur massa eu pharetra porttitor nulla ornare. Cursus senectus id ipsum cras cursus. Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu. 
@@ -10,61 +11,64 @@ export default function Page() {
   Cursus senectus id ipsum cras cursus. Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu. Lorem ipsum dolor sit amet consectetur. Consectetur massa eu pharetra porttitor nulla ornare. 
   Cursus senectus id ipsum cras cursus. Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu.`;
   return (
-    <div className="flex flex-col gap-2 my-4 p-4">
-      {/* Hero Section */}
-      <div className="flex flex-col">
-        <HeroSection
-          className=""
-          title="ACHIEVEMENTS"
-          description=""
-          imageUrl="/achievements-hero-image.jpg"
-          alt=""
-          showStats={true}
-        />
-      </div>
-      {/* Content Sections */}
-      <div className="flex flex-col space-y-10 mb-24">
-        <div>
-          <ContentCard className="" title="Schools Reached" content={content} />
-        </div>
-        <div className="grid grid-cols-2 gap-6">
-          <div className="relative aspect-[4/3] w-full">
-            <PictureCard
-              className=""
-              imageSrc="/achievements-image-1.jpg"
-              altText=""
-            />
-          </div>
-          <div className="relative aspect-[4/3] w-full">
-            <PictureCard
-              className=""
-              imageSrc="/achievements-image-2.jpg"
-              altText=""
-            />
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col space-y-10 mb-24">
-        <div>
-          <ContentCard className="" title="Schools Reached" content={content} />
-        </div>
-        <div className="grid grid-cols-2 gap-6">
-          <div className="relative aspect-[4/3] w-full">
-            <PictureCard
-              className=""
-              imageSrc="/achievements-image-2.jpg"
-              altText=""
-            />
-          </div>
-          <div className="relative aspect-[4/3] w-full">
-            <PictureCard
-              className=""
-              imageSrc="/achievements-image-1.jpg"
-              altText=""
-            />
-          </div>
-        </div>
-      </div>
+    <div>
+      <UnderConstruction />
     </div>
+    // <div className="flex flex-col gap-2 my-4 p-4">
+    //   {/* Hero Section */}
+    //   <div className="flex flex-col">
+    //     <HeroSection
+    //       className=""
+    //       title="ACHIEVEMENTS"
+    //       description=""
+    //       imageUrl="/achievements-hero-image.jpg"
+    //       alt=""
+    //       showStats={true}
+    //     />
+    //   </div>
+    //   {/* Content Sections */}
+    //   <div className="flex flex-col space-y-10 mb-24">
+    //     <div>
+    //       <ContentCard className="" title="Schools Reached" content={content} />
+    //     </div>
+    //     <div className="grid grid-cols-2 gap-6">
+    //       <div className="relative aspect-[4/3] w-full">
+    //         <PictureCard
+    //           className=""
+    //           imageSrc="/achievements-image-1.jpg"
+    //           altText=""
+    //         />
+    //       </div>
+    //       <div className="relative aspect-[4/3] w-full">
+    //         <PictureCard
+    //           className=""
+    //           imageSrc="/achievements-image-2.jpg"
+    //           altText=""
+    //         />
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div className="flex flex-col space-y-10 mb-24">
+    //     <div>
+    //       <ContentCard className="" title="Schools Reached" content={content} />
+    //     </div>
+    //     <div className="grid grid-cols-2 gap-6">
+    //       <div className="relative aspect-[4/3] w-full">
+    //         <PictureCard
+    //           className=""
+    //           imageSrc="/achievements-image-2.jpg"
+    //           altText=""
+    //         />
+    //       </div>
+    //       <div className="relative aspect-[4/3] w-full">
+    //         <PictureCard
+    //           className=""
+    //           imageSrc="/achievements-image-1.jpg"
+    //           altText=""
+    //         />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
