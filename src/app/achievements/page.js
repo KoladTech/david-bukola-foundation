ab1ebc1a07301cd553840119e124defd6b98dbe1
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2024-11-23 17:11:53 +0100

    Create achievements page
    Created the achievements page with the stats for the hero section pulled from firestore.
    Also added a loading spinner for wait times before the data is retrieved

--- src/app/achievements/page.js
+++ src/app/achievements/page.js
@@ -1,6 +1,11 @@
+"use client";
 import ContentCard from "@/components/ContentCard";
 import HeroSection from "@/components/HeroSection";
+import LoadingSpinner from "@/components/loadingSpinner";
 import PictureCard from "@/components/PictureCard";
+import db from "@/firebase/firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
+import { useState, useEffect } from "react";
 
 export default function Page() {
   let content = `Lorem ipsum dolor sit amet consectetur. Consectetur massa eu pharetra porttitor nulla ornare. Cursus senectus id ipsum cras cursus. Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu. 
@@ -9,18 +14,46 @@ export default function Page() {
   Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu.Lorem ipsum dolor sit amet consectetur. Consectetur massa eu pharetra porttitor nulla ornare. 
   Cursus senectus id ipsum cras cursus. Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu. Lorem ipsum dolor sit amet consectetur. Consectetur massa eu pharetra porttitor nulla ornare. 
   Cursus senectus id ipsum cras cursus. Nunc sed eu amet purus. Diam aliquet enim lectus etiam aliquam eu.`;
+
+  const [stats, setStats] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchStats() {
+      try {
+        const statsDoc = await getDoc(
+          doc(db, "Site Statistics", "jZGeXcoSIbk6pGTNXlln")
+        );
+        if (statsDoc.exists()) {
+          setStats(statsDoc.data()); // Populate stats from Firestore
+        }
+      } catch (error) {
+        console.error("Error fetching stats:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchStats();
+  }, []);
+
   return (
     <div className="flex flex-col gap-2 my-4 p-4">
       {/* Hero Section */}
       <div className="flex flex-col">
-        <HeroSection
-          className=""
-          title="ACHIEVEMENTS"
-          description=""
-          imageUrl="/achievements-hero-image.jpg"
-          alt=""
-          showStats={true}
-        />
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <HeroSection
+            className=""
+            title="ACHIEVEMENTS"
+            description=""
+            imageUrl="/achievements-hero-image.jpg"
+            alt=""
+            showStats={true}
+            stats={stats}
+          />
+        )}
       </div>
       {/* Content Sections */}
       <div className="flex flex-col space-y-10 mb-24">
