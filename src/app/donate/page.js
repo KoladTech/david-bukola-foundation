f0070630117930f482b7521215bb9d861f7a9f0a
sirKay1010 <sirkay1001@gmail.com>
2024-12-14 13:59:52 +0100

    feat(donate): add click functionality to cards
    - Added functionality to select a particular amount to donate OR make use of typing other amount
    - Added functionality to disable NEXT button until amount selected (for amount card-step) and email and names filled (for personal information card-step)

--- src/app/donate/page.js
+++ src/app/donate/page.js
@@ -1,9 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { ArrowRight, ChevronLeft } from "lucide-react";
 import HeroSection from "@/components/HeroSection";
 
 const donationAmounts = [
@@ -21,17 +22,20 @@ const otherWays = [
   "Donate via USSD",
   "Others",
 ];
+const totalSteps = 3;
 
 export default function DonatePage() {
   const [currentStep, setCurrentStep] = useState(0);
   const [selectedAmount, setSelectedAmount] = useState(null);
+  const [otherAmount, setOtherAmount] = useState(false);
+  const [inputMade, setInputMade] = useState(false);
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
     email: "",
     phone: "",
-    address: "",
     paymentMethod: "",
+    amount: "",
   });
 
   // Handles logic of going to next stage only after selecting a donation to give
@@ -40,24 +44,75 @@ export default function DonatePage() {
       setCurrentStep(currentStep + 1);
     }
   };
+  // Back Navigation on payment flow
   const handleBack = () => {
     if (selectedAmount) {
       setCurrentStep(Math.max(0, currentStep - 1));
     }
   };
+
+  // Handles getting data from input fields and populating the forData
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
       ...prev,
       [name]: value,
     }));
+    setInputMade(true);
   };
+  // UseEffect to ensure the setAmount is updated properly as the state changes
+  useEffect(() => {
+    if (inputMade) {
+      inputRef.current.value === "" && !selectedAmount
+        ? setSelectedAmount("")
+        : setSelectedAmount(formData.amount);
+    }
+  });
+
+  // Set the input field to be used to get the amount
+  const useOtherAmount = () => {
+    setOtherAmount(true);
+    if (inputRef.current.value === "") {
+      setSelectedAmount("");
+    }
+  };
+
+  // references for input fields
+  const inputRef = useRef(null);
+
+  // Create a single ref object
+  const formDataRefs = useRef({});
+
+  // const firstNameRef = useRef(null);
+  // const lastNameRef = useRef(null);
+  // const emailRef = useRef(null);
+
+  // Clears input field
+  const clearInput = (e) => {
+    setInputMade(false); // Make the input made button false (To disable next)
+    setOtherAmount(false); // disable otherAmount field
+    // clear the amount input field via the ref of the field
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
+
+  // Prevents default submission of forms
   const handleFormSubmit = (e) => {
     e.preventDefault();
     // Handle form submission (e.g., send data to Firestore or an API)
     console.log("Form Data Submitted:", formData);
     alert("Thank you for donating!");
   };
+
+  // Debugging!!!
+  useEffect(() => {
+    console.log(formData);
+    console.log(`Input Made ${inputMade}`);
+    console.log(`Input Made Value: ${inputRef.current.value}`);
+    console.log(`other amount ${otherAmount}`);
+    console.log(`selected Amount ${selectedAmount}`);
+  });
 
   return (
     // <form onSubmit={handleFormSubmit}>
@@ -78,11 +133,40 @@ export default function DonatePage() {
 
       {/* Form Section */}
       <div className="full-width-div ">
-        <div className="max-w-6xl mx-auto px-4 py-12 bg-sky-900">
+        <div className="max-w-6xl mx-auto px-4 py-12 bg-sky-800">
           <div className="flex flex-col md:flex-row gap-8 py-12">
             {/* Main Form Area */}
             <div className="flex-1">
               <Card className="bg-white p-8">
+                {/* Back Navigation arrow  */}
+                <div className="flex flex-col">
+                  <div>
+                    {currentStep > 0 && (
+                      <button
+                        onClick={handleBack}
+                        className="hover:text-sky-600 transition-colors"
+                      >
+                        <ChevronLeft className="w-6 h-6" />
+                      </button>
+                    )}
+                  </div>
+                  {/* Progress Indicators */}
+                  <div className="flex-1">
+                    <div className="flex justify-start gap-2 mb-6">
+                      {[...Array(totalSteps)].map((_, index) => (
+                        <div
+                          key={index}
+                          className={`w-4 h-4 rounded-full ${
+                            index === currentStep
+                              ? "bg-blue-600"
+                              : "bg-gray-500/40 border-blue-950"
+                          }`}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                {/* Card Texts div starts  */}
                 <div className="overflow-hidden">
                   <div
                     className="flex transition-transform duration-500 ease-in-out"
@@ -114,11 +198,22 @@ export default function DonatePage() {
                                   : "outline"
                               }
                               className={`bg-sky-200 h-12 text-lg ${
-                                selectedAmount === amount.value
-                                  ? "bg-sky-900"
-                                  : "border-white/40 text-white hover:bg-sky-900"
+                                selectedAmount === amount.value &&
+                                otherAmount === false
+                                  ? "bg-sky-800"
+                                  : "border-white/40 text-white hover:bg-sky-800"
                               }`}
-                              onClick={() => setSelectedAmount(amount.value)}
+                              // Onclciking any amount button
+                              onClick={() => {
+                                // alert([selectedAmount, otherAmount]);
+                                clearInput(); //Clear the input field
+                                setSelectedAmount(amount.value); //set that buttons amount
+                                // Set the value of the button into the form data
+                                setFormData((prev) => ({
+                                  ...prev,
+                                  amount: amount.value,
+                                }));
+                              }}
                             >
                               {amount.label}
                             </Button>
@@ -127,18 +222,21 @@ export default function DonatePage() {
                         {/* Other amount  */}
                         <div>
                           <input
-                            type="text"
-                            id="firstName"
-                            name="firstName"
+                            className="input-field"
+                            type="number"
+                            id="amount"
+                            name="amount"
                             placeholder="Other Amount"
                             value={FormData.amount}
-                            onChange={handleInputChange}
-                            className="input-field"
+                            ref={inputRef}
+                            onClick={useOtherAmount} //Enable otherAmount field
+                            onInput={(e) => {
+                              handleInputChange(e); //Call function to Handle input data
+                            }}
                           />
                         </div>
-
                         <Button
-                          className="w-full h-12 bg-sky-900 hover:bg-sky-800"
+                          className="w-full h-12 bg-sky-800 hover:bg-sky-700"
                           onClick={handleNext}
                           disabled={!selectedAmount}
                         >
@@ -163,6 +261,9 @@ export default function DonatePage() {
                                 name="firstName"
                                 value={FormData.firstName}
                                 onChange={handleInputChange}
+                                ref={(el) =>
+                                  (formDataRefs.current.firstName = el)
+                                }
                                 required
                                 placeholder="First Name * "
                                 className="input-field"
@@ -175,6 +276,10 @@ export default function DonatePage() {
                                 name="lastName"
                                 value={FormData.lastName}
                                 onChange={handleInputChange}
+                                // ref={lastNameRef}
+                                ref={(el) =>
+                                  (formDataRefs.current.lastName = el)
+                                }
                                 required
                                 placeholder="Last Name * "
                                 className="input-field"
@@ -189,6 +294,8 @@ export default function DonatePage() {
                               name="email"
                               value={FormData.email}
                               onChange={handleInputChange}
+                              // ref={emailRef}
+                              ref={(el) => (formDataRefs.current.email = el)}
                               required
                               placeholder="Email * "
                               className="input-field"
@@ -236,18 +343,18 @@ export default function DonatePage() {
                         <div className="flex gap-4">
                           <Button
                             variant="outline"
-                            className="flex-1 h-12 border-sky-900 text-sky-900"
+                            className="flex-1 h-12 border-sky-800 text-sky-800"
                             onClick={handleBack}
                           >
                             Back
                           </Button>
                           <Button
-                            className="flex-1 h-12 bg-sky-900 hover:bg-sky-800"
+                            className="flex-1 h-12 bg-sky-800 hover:bg-sky-700"
                             onClick={handleNext}
                             disabled={
-                              !formData.firstName &&
-                              !formData.firstName &&
-                              !formData.email
+                              !formDataRefs.current.firstName?.value ||
+                              !formDataRefs.current.lastName?.value ||
+                              !formDataRefs.current.email?.value
                             }
                           >
                             Next
@@ -273,9 +380,9 @@ export default function DonatePage() {
                             variant={`${
                               formData.paymentMethod === "card"
                             } ? "default" : "outline"`}
-                            className={`w-full justify-start text-left h-auto p-4 border-sky-900 ${
+                            className={`w-full justify-center text-left h-auto p-4 bg-sky-800 hover:bg-sky-950 ${
                               formData.paymentMethod === "card"
-                                ? "bg-sky-900"
+                                ? "bg-sky-600"
                                 : ""
                             }`}
                             onClick={() => {
@@ -285,11 +392,12 @@ export default function DonatePage() {
                               }));
                             }}
                           >
-                            <div>
-                              <div className="font-semibold text-sky-900">
+                            <div className="">
+                              <div className="flex justify-center font-semibold text-sky-300">
                                 Pay with Card
                               </div>
-                              <div className="text-sm text-gray-600">
+                              {/* Delete */}
+                              <div className="flex justify-center text-sm text-sky-100">
                                 Debit or Credit Card
                               </div>
                             </div>
@@ -300,10 +408,10 @@ export default function DonatePage() {
                             variant={`${
                               formData.paymentMethod === "transfer"
                             } ? "default" : "outline"`}
-                            className={`w-full justify-start text-left h-auto p-4 ${
+                            className={`w-full justify-center text-left h-auto p-4 bg-sky-800 hover:bg-sky-950 ${
                               formData.paymentMethod === "transfer"
-                                ? "bg-sky-900"
-                                : "border-white/40 text-white hover:bg-sky-900"
+                                ? "bg-sky-600"
+                                : ""
                             }`}
                             onClick={() => {
                               setFormData((prev) => ({
@@ -313,43 +421,64 @@ export default function DonatePage() {
                             }}
                           >
                             <div>
-                              <div className="font-semibold text-sky-900">
+                              <div className="flex justify-center font-semibold text-sky-300">
                                 Bank Transfer
                               </div>
-                              <div className="text-sm text-gray-600">
+                              <div className="flex justify-center text-sm text-sky-100">
                                 Direct bank transfer
                               </div>
                             </div>
                           </Button>
                         </div>
+
+                        {/* Payment confirmation buttons */}
                         <div className="flex gap-4">
                           <Button
                             variant="outline"
-                            className="flex-1 h-12 border-sky-900 text-sky-900"
+                            className="flex-1 h-12 text-white bg-sky-600"
+                            // onClick={handleBack}
+                          >
+                            I have made payment
+                          </Button>
+                          <Button
+                            variant="outline"
+                            className="flex-1 h-12 border-red-800 text-red-800"
+                            // onClick={handleNext}
+                            // disabled={!formData.paymentMethod}
+                          >
+                            Payment did not go through
+                          </Button>
+                        </div>
+
+                        {/* Navigation Buttons */}
+                        <div className="flex gap-4">
+                          <Button
+                            variant="outline"
+                            className="flex-1 h-12 border-sky-800 text-sky-800"
                             onClick={handleBack}
                           >
                             Back
                           </Button>
-                          <Button
-                            className="flex-1 h-12 bg-sky-900 text-white hover:bg-sky-800"
+                          {/* <Button
+                            className="flex-1 h-12 bg-sky-800 text-white hover:bg-sky-950"
                             onClick={handleNext}
                             disabled={!formData.paymentMethod}
                           >
                             Next
-                          </Button>
+                          </Button> */}
                         </div>
                       </div>
                     </div>
-                    {/* Card-step-4 Last Page */}
-                    <div>
+                    {/* Card-step-4 Last Page (IF WE IMPLEMENT TRANSFER MANUALLY)*/}
+                    {/* <div>
                       <Button
                         variant="outline"
-                        className="w-full h-12 border-sky-900 text-sky-900"
+                        className="w-full h-12 border-sky-800 text-sky-800"
                         onClick={handleBack}
                       >
                         Back
                       </Button>
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               </Card>
